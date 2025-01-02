9b11a38ac60ed8b90071574cd6646ed8ca8e6350
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2025-01-02 11:11:38 +0100

    Switch to git submodule in stead of npm for alfa

--- index.js
+++ index.js
@@ -1,100 +1,12 @@
-import { Outcome } from "@siteimprove/alfa-act";
-import { Node } from "@siteimprove/alfa-aria";
-import { Element, Text } from "@siteimprove/alfa-dom";
-import { Rules } from "@siteimprove/alfa-rules";
-import { Page } from "@siteimprove/alfa-web";
-import { Style } from "@siteimprove/alfa-style";
+import { AlfaDevTools } from "./alfa-dev-tools.js";
 
-class AlfaDevTools {
-  constructor(pageJson) {
-    this.page = Page.from(JSON.parse(pageJson)).getUnsafe();
-  }
-
-  isFailed = Outcome.isFailed;
-  isPassed = Outcome.isPassed;
-  isCantTell = Outcome.isCantTell;
-  isElement = Element.isElement;
-  isText = Text.isText;
-  hasName = Element.hasName;
-  isVisible = Style.isVisible;
-
-  async audit(rule) {
-    return await Rules.get(rule).getUnsafe().evaluate(this.page);
-  }
-
-  summarize(rule) {
-    this.audit(rule).then((result) => {
-      const cantTell = result.filter(Outcome.isCantTell);
-      const passed = result.filter(Outcome.isPassed);
-      const failed = result.filter(Outcome.isFailed);
-
-      console.log(
-        `cantTell: ${cantTell.length}, passed: ${passed.length}, failed: ${failed.length}`,
-      );
-    });
-  }
-
-  getElements() {
-    return this.page.document.descendants().filter(Element.isElement);
-  }
-
-  getElementById(id) {
-    return this.page.document
-      .descendants()
-      .filter(Element.isElement)
-      .find((element) => element.id.includes(id))
-      .getOr(undefined);
-  }
-
-  getElementsByClassName(className) {
-    return this.page.document
-      .descendants()
-      .filter(Element.isElement)
-      .filter((element) => element.classes.includes(className));
-  }
-
-  getElementsByTagName(tagName) {
-    return this.page.document
-      .descendants()
-      .filter(Element.isElement)
-      .filter(Element.hasName(tagName));
-  }
-
-  getElementsByText(str) {
-    return this.page.document
-      .descendants()
-      .filter(Text.isText)
-      .filter((text) => text.data.includes(str))
-      .map((text) => text.parent().getUnsafe())
-      .filter(Element.isElement);
-  }
-
-  getComputedStyle(element, property) {
-    return Style.from(element, this.page.device).computed(property);
-  }
-
-  getAriaNode(element) {
-    return Node.from(element, this.page.device);
-  }
-
-  createDomElement(element) {
-    const domElement = document.createElement(element.name);
-
-    for (let attr of element.attributes) {
-      domElement.setAttribute(attr.name, attr.value);
-    }
-
-    for (let child of element.children()) {
-      if (Text.isText(child)) {
-        domElement.append(child.data);
-      } else {
-        domElement.append(this.createDomElement(child));
-      }
-    }
-
-    return domElement;
-  }
-}
+import * as act from "@siteimprove/alfa-act";
+import * as aria from "@siteimprove/alfa-aria";
+import * as dom from "@siteimprove/alfa-dom";
+import * as rules from "@siteimprove/alfa-rules";
+import * as web from "@siteimprove/alfa-web";
+import * as sequence from "@siteimprove/alfa-sequence";
+import * as style from "@siteimprove/alfa-style";
 
 const storedPageJson = localStorage.getItem("page.json");
 
@@ -108,19 +20,27 @@ if (storedPageJson) {
   }
 }
 
-function setGlobalAlfa() {
-  window.alfa = {};
+function init() {
+  window.alfa = {
+    act,
+    aria,
+    dom,
+    rules,
+    web,
+    sequence,
+    style,
+  };
 
-  window.alfa = new AlfaDevTools(document.getElementById("page").value);
+  window.alfaDT = new AlfaDevTools(document.getElementById("page").value);
 }
 
-setGlobalAlfa();
+init();
 
-document.getElementById("page").addEventListener("keyup", setGlobalAlfa);
+document.getElementById("page").addEventListener("keyup", init);
 window.addEventListener("beforeunload", () => {
   localStorage.setItem("page.json", document.getElementById("page").value);
 });
 
 console.log(
-  "Welcome to Alfa in DevTools! Get started by inspecting the global \`alfa\` instance.",
+  "Welcome to Alfa in DevTools! Get started by inspecting the global \`alfa\` and \`alfaDT\` instances.",
 );
